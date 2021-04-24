8e00a12dda31fe9ee93cb1d311de9df62a742137
tobire <mail@tobias-reinke.de>
2021-04-24 12:52:59 +0200

    added gametime

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import asyncio
-import datetime
+from datetime import datetime
 import random
 import websockets
+import time as timing
 from threading import Thread
+from threading import Timer
 
-startTime = 30
+shotclockStart = 30
 rooms = {}
 
 async def time(websocket, path):
   room = path[1:]
   if room not in rooms:
-    rooms[room] = { 'time': startTime, 'running': False, 'clients': set() }
+    rooms[room] = { 'game': 0, 'shotclock': shotclockStart, 'running': False, 'clients': set(), 'active': True }
+  elif not rooms[room]['active']:
+    rooms[room]['active'] = True
+    log(room + " reactivated")
   rooms[room]['clients'].add(websocket)
   await sendTimeToClient(websocket, room)
 
-  print(getName(websocket) + ' joined ' + room)
+  log(getName(websocket) + ' joined ' + room)
   try:
     while True:
       async for message in websocket:
-        print(getName(websocket) + '@' + room + ': ' + message)
+        log(getName(websocket) + '@' + room + ': ' + message)
         if message == 'start':
           await startTimer(room)
         elif message == 'stop':
@@ -34,30 +39,44 @@ def getName(ws):
   return websocket.remote_address[0]
 
 def disconnected(ws, room):
-  print(getName(ws) + ' left ' + room)
+  log(getName(ws) + ' left ' + room)
   clients = rooms[room]['clients'].copy()
   clients.remove(ws)
   rooms[room]['clients'] = clients
-  if len(rooms[room]['clients']) == 0:
+  if len(rooms[room]['clients']) == 0 and rooms[room]['active']:
+    rooms[room]['active'] = False
+    log(room + ' deactivated')
+    Timer(300.0, deleteRoom, (room,)).start()
+
+def deleteRoom(room):
+  if not rooms[room]['active']:
     del(rooms[room])
-    print(room + ' closed')
+    log(room + ' deleted')
+  else:
+    log('Skipping delete for ' + room)
 
 async def startTimer(room):
-  rooms[room]['running'] = True
-  thread = Thread(target=asyncio.run, args=(timerThread(room),))
-  thread.start()
+  if not rooms[room]['running']:
+    rooms[room]['running'] = True
+    thread = Thread(target=asyncio.run, args=(timerThread(room),))
+    thread.start()
 
 async def timerThread(room):
-  while room in rooms and rooms[room]['running'] and rooms[room]['time'] > 0:
-    rooms[room]['time'] = rooms[room]['time'] - 1
+  while room in rooms and rooms[room]['running']:
+    t1 = timing.time()
+    rooms[room]['game'] = rooms[room]['game'] + 1
+    if rooms[room]['shotclock'] > 0:
+      rooms[room]['shotclock'] = rooms[room]['shotclock'] - 1
     await sendTimeToClients(room)
-    await asyncio.sleep(1)
+    t2 = timing.time()
+    diff = t2 - t1
+    await asyncio.sleep(1 - diff)
 
 def stopTimer(room):
   rooms[room]['running'] = False
 
 async def resetTimer(room):
-  rooms[room]['time'] = startTime
+  rooms[room]['shotclock'] = shotclockStart
   await sendTimeToClients(room)
 
 async def sendTimeToClients(room):
@@ -66,10 +85,14 @@ async def sendTimeToClients(room):
 
 async def sendTimeToClient(ws, room):
   try:
-    time = rooms[room]['time']
-    await ws.send('time:' + str(time))
+    shotclock = rooms[room]['shotclock']
+    game = rooms[room]['game']
+    await ws.send('g' + str(game) + ';s' + str(shotclock))
   except:
     disconnected(ws, room)
+
+def log(msg):
+  print(str(datetime.now()) + ": " + msg)
 
 start_server = websockets.serve(time, "localhost", 5678)
 
